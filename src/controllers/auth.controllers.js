00866fd30cc882190388af67a635645331c794e0
Sohan Chaudhari <169914576+sohanasz@users.noreply.github.com>
2025-06-06 16:09:07 +0530

    Fixed password not getting hashed

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -29,11 +29,13 @@ const registerUser = async (req, res) => {
       });
     }
 
+    const hashedPassword = bcrypt.hash(password, 12)
+
     const user = await User.create({
       username,
       role: role || "member",
       email,
-      password,
+      password: hashedPassword,
     });
     console.log("USER - ", user);
 
